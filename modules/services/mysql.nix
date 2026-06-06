99e19b0b7b3356a63fdbba12773df28e4d924eeb
Adam0 <alumail@protonmail.com>
2026-06-06 22:31:02 +0200

    configure databases

--- modules/services/mysql.nix
+++ modules/services/mysql.nix
@@ -3,11 +3,25 @@
     services.mysql = {
       enable = true;
       package = pkgs.mariadb;
+
+      settings = {
+        mysqld = {
+          # TODO(remote access): Revisit database exposure once the access model is decided.
+          bind-address = "127.0.0.1";
+          innodb_buffer_pool_size = "512M";
+          max_connections = 100;
+          slow_query_log = true;
+          long_query_time = 1;
+        };
+
+        mysqldump.max_allowed_packet = "64M";
+      };
     };
 
     services.mysqlBackup = {
       enable = true;
       calendar = "02:00:00";
+
       compressionAlg = "zstd";
       compressionLevel = 3;
     };
--- modules/services/postgres.nix
+++ modules/services/postgres.nix
@@ -1,17 +1,43 @@
 {
-  flake.modules.nixos.postgres = {pkgs, ...}: {
+  flake.modules.nixos.postgres = {
+    # keep-sorted start
+    lib,
+    pkgs,
+    # keep-sorted end
+    ...
+  }: let
+    inherit (lib) mkForce;
+  in {
     services.postgresql = {
       enable = true;
       enableTCPIP = true;
 
       package = pkgs.postgresql_18;
+
+      settings = {
+        # TODO(remote access): Revisit database exposure once the access model is decided.
+        listen_addresses = mkForce "127.0.0.1,::1";
+
+        shared_buffers = "256MB";
+        maintenance_work_mem = "128MB";
+        log_connections = true;
+        log_disconnections = true;
+        log_min_duration_statement = 1000;
+      };
+
+      authentication = ''
+        local all all peer
+        host all all 127.0.0.1/32 scram-sha-256
+        host all all ::1/128 scram-sha-256
+      '';
     };
 
     services.postgresqlBackup = {
       enable = true;
+      startAt = "*-*-* 02:00:00";
+
       compression = "zstd";
       compressionLevel = 3;
-      startAt = "*-*-* 02:00:00";
     };
   };
 }
