6686044cd59700d7b3294d339658cf183ffadde6
Adam0 <alumail@protonmail.com>
2026-06-09 07:37:12 +0200

    update gotify to nightly

--- modules/services/gotify.nix
+++ modules/services/gotify.nix
@@ -1,20 +1,29 @@
 {
-  flake.modules.nixos.gotify = {lib, ...}: let
+  flake.modules.nixos.gotify = {
+    # keep-sorted start
+    lib,
+    pkgs,
+    # keep-sorted end
+    ...
+  }: let
     inherit (lib) mkForce;
   in {
     services.gotify = {
       enable = true;
+      package = pkgs.nur.repos.adam0.gotify-server;
       stateDirectoryName = "gotify";
 
       environment = {
         GOTIFY_SERVER_PORT = 44407;
-        GOTIFY_DATABASE_CONNECTION = "host=/run/postgresql user=gotify dbname=gotify sslmode=disable";
+
         GOTIFY_DATABASE_DIALECT = "postgres";
+        GOTIFY_DATABASE_CONNECTION = "host=/run/postgresql user=gotify dbname=gotify sslmode=disable";
       };
     };
 
     users = {
       groups.gotify = {};
+
       users.gotify = {
         group = "gotify";
         isSystemUser = true;
