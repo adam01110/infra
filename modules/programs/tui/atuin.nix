dda6a257a9bc8f363b47fdf98a5a27010e97a9b1
Adam0 <alumail@protonmail.com>
2026-08-18 18:26:05 -0400

    Improve Atuin integration and secret filtering

--- modules/programs/tui/atuin.nix
+++ modules/programs/tui/atuin.nix
@@ -17,6 +17,7 @@
       logLevel = "error";
     in {
       enable = true;
+      enableFishIntegration = true;
       forceOverwriteSettings = true;
 
       # keep-sorted start block=yes newline_separated=yes
@@ -49,9 +50,14 @@
         # keep-sorted start block=yes newline_separated=yes
         history_filter = [
           # keep-sorted start
+          ''(?i)authorization:[[:space:]]+(bearer|basic|token|apikey|api[_-]?key|access[_-]?token)[[:space:]=]+''
           ''(^|[[:space:]])(export[[:space:]]+)?[A-Z_][A-Z0-9_]*(_TOKEN|_SECRET|_PASSWORD|_PASSWD|_API_KEY|_ACCESS_KEY|_SECRET_ACCESS_KEY)(="[^"]+"|=[^[:space:]]+)''
           ''(^|[[:space:]])--?(token|secret|password|passwd|api[-_]?key|access[-_]?key|client[-_]?secret)(=|[[:space:]]+)''
-          ''(sk-[A-Za-z0-9_-]{20,}|ghp_[A-Za-z0-9]{20,}|github_pat_[A-Za-z0-9_]{20,}|glpat-[A-Za-z0-9_-]{20,}|AKIA[0-9A-Z]{16})''
+          ''(sk-[A-Za-z0-9_-]{20,}|[sp]k_(live|test)_[A-Za-z0-9]{20,}|rk_live_[A-Za-z0-9]{20,}|ghp_[A-Za-z0-9]{20,}|github_pat_[A-Za-z0-9_]{20,}|glpat-[A-Za-z0-9_-]{20,}|AKIA[0-9A-Z]{16}|xox[baprs]-[A-Za-z0-9-]{10,}|SG\.[A-Za-z0-9_\-\.]{20,}|AIza[A-Za-z0-9_\-]{30,}|ya29\.[A-Za-z0-9_\-]{10,}|nvapi-[A-Za-z0-9_-]{20,}|hf_[A-Za-z0-9]{20,})''
+          ''-----BEGIN (RSA |EC |DSA |OPENSSH |PGP |ENCRYPTED )?PRIVATE KEY-----''
+          ''[?&][a-z0-9_-]*(secret|token|key|passwd|password|apikey|api[_-]?key|access[_-]?token|refresh[_-]?token|signature|session[_-]?key|credential)[=][^[:space:]&'"<>]+''
+          ''[a-z][a-z0-9+.-]*://[^/@[:space:]'"]+:[^@[:space:]'"]+@''
+          ''eyJ[A-Za-z0-9_-]{10,}\.[A-Za-z0-9_-]{10,}\.[A-Za-z0-9_-]{10,}''
           # keep-sorted end
         ];
 
