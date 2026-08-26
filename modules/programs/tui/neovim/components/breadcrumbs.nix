0046864f7c858b9417e70f5d80d0d17473a181be
Adam0 <alumail@protonmail.com>
2026-08-26 23:09:57 +0200

    Use dropbar breadcrumb navigation

--- modules/programs/tui/neovim/components/breadcrumbs.nix
+++ modules/programs/tui/neovim/components/breadcrumbs.nix
@@ -1,21 +1,5 @@
 {
   flake.modules.homeManager.neovim = {
-    programs.nvf.settings.vim = {
-      # keep-sorted start block=yes newline_separated=yes
-      keymaps = [
-        {
-          key = "<leader>nv";
-          mode = "n";
-          action = "<cmd>Navbuddy<cr>";
-          desc = "Open Navbuddy";
-        }
-      ];
-
-      statusline.lualine.integrations.breadcrumbs = {
-        location = "winbar";
-        navbuddy.enable = true;
-      };
-      # keep-sorted end
-    };
+    programs.nvf.settings.vim.ui.dropbar-nvim.enable = true;
   };
 }
