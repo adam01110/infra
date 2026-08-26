80a54809622063ae5c2f6d692e4f24c71e4d975c
Adam0 <alumail@protonmail.com>
2026-08-26 15:58:07 -0400

    Modernize Neovim breadcrumbs

--- modules/programs/tui/neovim/components/breadcrumbs.nix
+++ modules/programs/tui/neovim/components/breadcrumbs.nix
@@ -1,8 +1,21 @@
 {
   flake.modules.homeManager.neovim = {
-    programs.nvf.settings.vim.ui.breadcrumbs = {
-      enable = true;
-      source = "dropbar";
+    programs.nvf.settings.vim = {
+      # keep-sorted start block=yes newline_separated=yes
+      keymaps = [
+        {
+          key = "<leader>nv";
+          mode = "n";
+          action = "<cmd>Navbuddy<cr>";
+          desc = "Open Navbuddy";
+        }
+      ];
+
+      statusline.lualine.integrations.breadcrumbs = {
+        location = "winbar";
+        navbuddy.enable = true;
+      };
+      # keep-sorted end
     };
   };
 }
