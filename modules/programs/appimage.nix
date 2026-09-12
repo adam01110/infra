8fdc7ab7db490d4ee48eb20ae6c5e0bec1ebd489
Adam0 <alumail@protonmail.com>
2026-09-12 22:29:29 +0200

    Add extra dependencies for appimage

--- modules/programs/appimage.nix
+++ modules/programs/appimage.nix
@@ -1,8 +1,15 @@
 {
-  flake.modules.nixos.appimage = {
+  flake.modules.nixos.appimage = {pkgs, ...}: {
     programs.appimage = {
       enable = true;
       binfmt = true;
+
+      # Add extra dependencies for appimages.
+      package = pkgs.appimage-run.override {
+        extraPkgs = pkgs: [
+          pkgs.icu
+        ];
+      };
     };
   };
 }
